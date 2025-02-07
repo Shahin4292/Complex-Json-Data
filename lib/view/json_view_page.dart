22c62743bfb513f343b645f3922db2d94ffc3fed
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2025-02-07 18:03:13 +0600

    update

--- lib/view/json_view_page.dart
+++ lib/view/json_view_page.dart
@@ -16,6 +16,7 @@ class _JsonViewPageState extends State<JsonViewPage> {
     final response = await http.get(Uri.parse(
         'https://api.edamam.com/search?q=All&app_id=b4569f96&app_key=2859517f694e1ed605f5a9e8829e80ca&from=0&to=20&calories=591-722&health=alcohol-free'));
     var data = jsonDecode(response.body.toString());
+    print(data);
     if (response.statusCode == 200) {
       return ProductsModel.fromJson(data);
     } else {
@@ -35,8 +36,43 @@ class _JsonViewPageState extends State<JsonViewPage> {
           Expanded(
               child: FutureBuilder<ProductsModel>(
                   future: getProductsApi(),
-                  builder: (context, index) {
-                    return SizedBox();
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                          itemCount: snapshot.data!.hits!.length,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                Container(
+                                  height:
+                                      MediaQuery.sizeOf(context).height * .3,
+                                  width: MediaQuery.sizeOf(context).width * .1,
+                                  child: ListView.builder(
+                                      itemCount: snapshot.data!.hits![index]
+                                          .recipe!.image!.length,
+                                      itemBuilder: (context, index) {
+                                        return Container(
+                                          height: MediaQuery.sizeOf(context)
+                                                  .height *
+                                              .25,
+                                          width:
+                                              MediaQuery.sizeOf(context).width *
+                                                  .07,
+                                          // decoration: BoxDecoration(
+                                          //     image: DecorationImage(
+                                          //         image: NetworkImage(snapshot.data!.hits![index].recipe!.image!.length.toString()))),
+                                          child: Text(snapshot.data!
+                                              .hits![index].recipe!.mealType
+                                              .toString()),
+                                        );
+                                      }),
+                                )
+                              ],
+                            );
+                          });
+                    } else {
+                      return Center(child: Text("Loading"));
+                    }
                   }))
         ],
       ),
